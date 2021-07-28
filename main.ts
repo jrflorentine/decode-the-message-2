27471dc9c63be092ac7e889899a2b5d3260d14c7
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-07-28 13:06:16 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,5 @@ basic.forever(function () {
     for (let index = 0; index <= message_array.length; index++) {
         radio.setGroup(index)
         radio.sendString("" + (message_array[index]))
-        basic.showString("" + (message_array[index]))
     }
 })
